e3e42b399c0de55dcd0c0936d87e131652f36408
Jade <jade.kam@protonmail.com>
2025-06-13 13:03:55 +0200

    merge + login/register

--- login_register/index.php
+++ login_register/index.php
@@ -1,3 +1,25 @@
+<?php
+
+session_start();
+
+$errors = [
+    'login' => $_SESSION['login_error'] ?? '',
+    'register' => $_SESSION['register_error'] ?? ''
+];
+$activeForm = $_SESSION['active_form'] ?? 'login';
+
+session_unset();
+
+function showError($error) {
+    return !empty($error) ? "<p class='error-message'>$error</p>" : '';
+}
+
+function isActiveForm($formName, $activeForm){
+    return $formName === $activeForm ? 'active' : '';
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,9 +32,10 @@
 
 <body>
     <div class="container">
-        <div class="form-box active" id="login-form">
+        <div class="form-box <?= isActiveForm('login', $activeForm); ?>" id="login-form">
             <form action = "login_register.php" method="post">
                 <h2>Connexion</h2>
+                <?= showError($errors['login']); ?>
                 <input type = "email" name = "email" placeholder="Email" required>
                 <input type = "password" name = "password" placeholder="Mot de passe" required>
                 <button type = "submit" name = "login">Se connecter</button>
@@ -20,9 +43,10 @@
             </form>
         </div>
 
-        <div class="form-box" id="register-form">
+        <div class="form-box <?= isActiveForm('register', $activeForm); ?>" id="register-form">
             <form action = "login_register.php" method="post">
                 <h2>Inscription</h2>
+                <?= showError($errors['register']); ?>
                 <input type = "text" name = "nom" placeholder="Nom" required>
                 <input type = "text" name = "prenom" placeholder="Prénom" required>
                 <input type = "email" name = "email" placeholder="Email" required>
--- login_register/login_register.php
+++ login_register/login_register.php
@@ -3,11 +3,50 @@
 session_start();
 require_once 'config.php';
 
-if (isset($_POST[''])){
-    $nom = $_POST[''];
-    $prenom = $_POST[''];
-    $email = $_POST[''];
-    $password = $_POST[''];
+if (isset($_POST['register'])){
+    $nom = $_POST['nom'];
+    $prenom = $_POST['prenom'];
+    $email = $_POST['email'];
+    $password = password_hash($_POST['password'],PASSWORD_DEFAULT);
+
+    $checkEmail = $conn->query("SELECT email FROM users WHERE email = '$email");
+    if ($checkEmail->num_rows>0){
+        $_SESSION['register_error'] = "Un compte associé à cet email existe déjà.";
+        $_SESSION['active_form'] = 'register';
+    } else {
+        $conn->query("INSERT INTO users (nom,prenom,email,password,role) VALUES ('$nom','$prenom', '$email','$password','user')");
+    }
+
+    header("Location: index.php");
+    exit();
+}
+
+if (isset($_POST['login'])){
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $result = $conn->query("SELECT * FROM users WHERE email = '$email'");
+
+    if ($result->num_rows>0){
+        $user = $result->fetch_assoc();
+        if (password_verify($password, $user['password'])){
+            $_SESSION['nom'] = $user['nom'];
+            $_SESSION['prenom'] = $user['prenom'];
+            $_SESSION['email'] = $user['email'];
+
+            if ($user['role']==='admin'){
+                header("Location: admin_page.php");
+            } else {
+                header("Location: user_page.php"); 
+            }
+            exit();
+        }
+    } 
+    $_SESSION['login_error'] = "L'email ou le mot de passe est incorrect.";
+    $_SESSION['active_form'] = 'login';
+
+    header("Location: index.php");
+    exit();
 }
 
 ?>
